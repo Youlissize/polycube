5c2d919e88d3e98309047f495316e6271b5ffb4c
ulysse <ulysse.jobard@telecom-paris.fr>
2020-05-09 12:01:09 +0200

    could now open tetraedral meshes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # polycube
 l1 based polycube maps - IGR 205
+
+/!\ Pour que les iconnes fonctionnent correctement :
+ - créer "fullpath.h"
+ - le remplir avec :
+	#include <QString>
+	const QString fullpath = QString("  ...chemin d'acces... /basicQGLProject/basicQGLProject");
---
+++ src/Mesh.h
@@ -0,0 +1,30 @@
+#ifndef PROJECTMESH_H
+#define PROJECTMESH_H
+
+#include <vector>
+#include "point3.h"
+
+
+
+struct Vertex{
+    point3d p;
+    Vertex() {}
+    Vertex(double x , double y , double z) : p(x,y,z) {}
+    double & operator [] (unsigned int c) { return p[c]; }
+    double operator [] (unsigned int c) const { return p[c]; }
+};
+struct Triangle{
+    unsigned int corners[3];
+    unsigned int & operator [] (unsigned int c) { return corners[c]; }
+    unsigned int operator [] (unsigned int c) const { return corners[c]; }
+    unsigned int size() const { return 3 ; }
+};
+struct Mesh{
+    std::vector< Vertex > vertices;
+    std::vector< Triangle > triangles;
+    std::vector< std::vector< int > > tetras;
+};
+
+
+
+#endif // PROJECTMESH_H
