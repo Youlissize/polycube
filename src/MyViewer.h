f6be9209993ebaacf5e9514e7f2849cbf3aaef4c
Lieunoir <lieunoir@rezel.net>
2020-06-04 16:37:33 +0200

    Fix hessian mistakes

--- src/MyViewer.h
+++ src/MyViewer.h
@@ -573,9 +573,15 @@ public slots:
                 }
 
                 Eigen::MatrixXd H_small = Eigen::MatrixXd(9,9);
+                for(int i = 0; i < 9; ++i) {
+                    for(int j = 0; j < 9; ++j) {
+                            H_small(i,j) = 0.0;
+                    }
+                }
                 //Compute c0, c1 and c2 hessian's first part
                 H_small += epsilon * (1/(c0b*c0b*c0b) * grad_c0 * grad_c0.transpose() + 1/(c1b*c1b*c1b) * grad_c1 * grad_c1.transpose() + 1/(c2b*c2b*c2b) * grad_c2 * grad_c2.transpose());
-                //Compute c0 hessian's second part                H_small(1,5) += c0/c0b;
+                //Compute c0 hessian's second part
+                H_small(1,5) += c0/c0b;
                 H_small(5,1) += c0/c0b;
                 H_small(1,8) += -c0/c0b;
                 H_small(8,1) += -c0/c0b;
@@ -585,8 +591,8 @@ public slots:
                 H_small(4,8) += c0/c0b;
                 H_small(8,4) += c0/c0b;
 
-                H_small(7,3) += c0/c0b;
-                H_small(3,7) += c0/c0b;
+                H_small(7,2) += c0/c0b;
+                H_small(2,7) += c0/c0b;
                 H_small(7,5) += -c0/c0b;
                 H_small(5,7) += -c0/c0b;
 
